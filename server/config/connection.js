b46c5ad4da685ea08bd3a5b6c6009a57057ca363
sourslaw <lawanscott@gmail.com>
2023-12-05 22:08:30 -0600

    No option

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,8 @@ require('dotenv').config({ path : path.resolve('../.env') });
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/pokemons_SHITTEST',
+  // process.env.MONGODB_URI || 'mongodb://localhost/pokemons_SHITTEST',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
