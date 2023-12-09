251895ebd7f96a109b3090afa74c4e5a904df984
sourslaw <lawanscott@gmail.com>
2023-12-08 22:20:31 -0600

    revert

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,10 @@
-const path = require('path');
-require('dotenv').config({ path : path.resolve('../.env') });
-
+// const path = require('path');
+// require('dotenv').config({ path : path.resolve('../.env') });
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  // process.env.MONGODB_URI || 'mongodb://localhost/pokemons_SHITTEST',
-  process.env.MONGODB_URI,
+  process.env.MONGODB_URI || 'mongodb://localhost/pokemons_SHITTEST',
+  // process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
