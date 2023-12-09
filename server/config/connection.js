7e966c673a82f761ae31fb83f19e27f6d4db68c2
sourslaw <lawanscott@gmail.com>
2023-12-08 21:07:46 -0600

    r

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const path = require('path');
-require('dotenv').config({ path : path.resolve('../.env') });
-const mongoose = require('mongoose');
+// require('dotenv').config({ path : path.resolve('../.env') });
+// const mongoose = require('mongoose');
 
 mongoose.connect(
   // process.env.MONGODB_URI || 'mongodb://localhost/pokemons_SHITTEST',
